09cbdf46d9f56b0462f5eb21fec0f3ffafd84eb1
Michal Chroazak <michochozaku@gmail.com>
2018-09-10 13:10:55 +0100

    #19 clang format

--- test/RequestHandlerTest.cpp
+++ test/RequestHandlerTest.cpp
@@ -14,8 +14,8 @@ TEST(RequestHandlerTest, subscribe_consume_n_last_messages_test) {
 
   RequestHandler NewRequestHandler(std::move(KafkaConnection));
 
-  EXPECT_NO_THROW(
-      NewRequestHandler.subscribeConsumeNLastMessages("ExampleTestTopic", 100,1));
+  EXPECT_NO_THROW(NewRequestHandler.subscribeConsumeNLastMessages(
+      "ExampleTestTopic", 100, 1));
 }
 
 TEST(RequestHandlerTest, subscribe_at_an_offset_test) {
