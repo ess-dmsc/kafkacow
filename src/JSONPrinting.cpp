99a52246e8491bfe6e1b3449869d752d56a6d5c5
Michal Chroazak <michochozaku@gmail.com>
2018-09-19 12:57:18 +0100

    Re #17 JSONPrinting.cpp doxygen

--- src/JSONPrinting.cpp
+++ src/JSONPrinting.cpp
@@ -4,11 +4,15 @@
 #include <nlohmann/json.hpp>
 #include <yaml-cpp/yaml.h>
 
+/// Receives deserialized flatbuffers message, formats it and prints it to the
+/// screen.
+///
+/// \param JSONMessage
 void JSONPrinting::printEntireMessage(const std::string &JSONMessage) {
-
   using std::cout;
   using nlohmann::json;
 
+  // convert into valid JSON
   YAML::Node node = YAML::Load(JSONMessage);
   YAML::Emitter Emitter;
   Emitter << YAML::DoubleQuoted << YAML::Flow << node;
@@ -24,6 +28,10 @@ void JSONPrinting::printEntireMessage(const std::string &JSONMessage) {
   cout << "\n__________________________________________________\n";
 }
 
+/// Receives deserialized flatbuffers message, truncated large arrays, formats
+/// it and prints it to the screen.
+///
+/// \param JSONMessage
 void JSONPrinting::printTruncatedMessage(const std::string &JSONMessage) {
   using std::cout;
   using nlohmann::json;
@@ -48,23 +56,26 @@ void JSONPrinting::printTruncatedMessage(const std::string &JSONMessage) {
   cout << "\n__________________________________________________\n";
 }
 
+/// First of a pair of recursive methods that receive YAML node and truncate
+/// long arrays that it contains.
+///
+/// \param Node
 void JSONPrinting::recursiveTruncateJSONMap(YAML::Node &Node) {
   for (YAML::const_iterator it = Node.begin(); it != Node.end(); ++it) {
-    std::vector<std::string> Values;
-    Values.push_back(it->first.as<std::string>());
     auto childNode = *it;
     if (it->second.IsMap()) {
       recursiveTruncateJSONMap(childNode.second);
     } else if (it->second.IsSequence()) {
       recursiveTruncateJSONSequence(childNode.second);
-    } else {
-      Values.push_back(it->second.as<std::string>());
     }
   }
 }
 
+/// First of a pair of recursive methods that receive YAML node and truncate
+/// long arrays that it contains.
+///
+/// \param Node
 void JSONPrinting::recursiveTruncateJSONSequence(YAML::Node &Node) {
-  std::vector<std::string> Values;
   int Counter = 0;
   auto Begin = Node.begin();
   auto End = Node.end();
