ab7c234229ca4df95f3f376fa8f36097fd8ec303
Matthew D Jones <matthew.d.jones@tessella.com>
2018-10-08 08:15:50 +0100

    #49 Pass ref instead of moving unique_ptr

--- src/ConnectKafka.cpp
+++ src/ConnectKafka.cpp
@@ -4,17 +4,15 @@
 
 namespace {
 
-std::unique_ptr<RdKafka::Conf>
-configAndLogError(std::unique_ptr<RdKafka::Conf> conf, std::string Arg1,
-                  std::string Arg2) {
+void setConfig(RdKafka::Conf &conf, const std::string &ConfigName,
+               const std::string &ConfigValue) {
   std::shared_ptr<spdlog::logger> Logger = spdlog::get("LOG");
   std::string ErrStr;
-  conf->set(Arg1, Arg2, ErrStr);
+  conf.set(ConfigName, ConfigValue, ErrStr);
   if (!ErrStr.empty()) {
     ErrStr.append(" in createGlobalConfiguration([...])");
     Logger->error(ErrStr);
   }
-  return conf;
 }
 
 std::unique_ptr<RdKafka::Conf>
@@ -22,18 +20,19 @@ createGlobalConfiguration(const std::string &BrokerAddr) {
   auto conf = std::unique_ptr<RdKafka::Conf>(
       RdKafka::Conf::create(RdKafka::Conf::CONF_GLOBAL));
 
-  conf = configAndLogError(std::move(conf), "metadata.broker.list", BrokerAddr);
-  conf = configAndLogError(std::move(conf), "session.timeout.ms", "10000");
-  conf = configAndLogError(std::move(conf), "group.id", "mantid");
-  conf = configAndLogError(std::move(conf), "message.max.bytes", "10000000");
-  conf =
-      configAndLogError(std::move(conf), "fetch.message.max.bytes", "10000000");
-  conf = configAndLogError(std::move(conf), "enable.auto.commit", "false");
-  conf =
-      configAndLogError(std::move(conf), "enable.auto.offset.store", "false");
-  conf = configAndLogError(std::move(conf), "offset.store.method", "none");
-  conf = configAndLogError(std::move(conf), "api.version.request", "true");
-  conf = configAndLogError(std::move(conf), "auto.offset.reset", "largest");
+  setConfig(*conf, "metadata.broker.list", BrokerAddr);
+  setConfig(*conf, "session.timeout.ms", "10000");
+  setConfig(*conf, "message.max.bytes", "10000000");
+  setConfig(*conf, "fetch.message.max.bytes", "10000000");
+  setConfig(*conf, "enable.auto.commit", "false");
+  setConfig(*conf, "enable.auto.offset.store", "false");
+  setConfig(*conf, "offset.store.method", "none");
+  setConfig(*conf, "api.version.request", "true");
+  setConfig(*conf, "auto.offset.reset", "largest");
+  // kafkacow uses assign not subscribe, so group id is not used for consumer
+  // balancing.
+  setConfig(*conf, "group.id", "kafkacow");
+
   return conf;
 }
 }
