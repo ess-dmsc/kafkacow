28cd1a70484e6c29278df947da025461d0d74f80
mchorazak <michochozaku@gmail.com>
2019-02-19 15:07:56 +0000

    re #74 fix tests

--- test/JSONPrintingTest.cpp
+++ test/JSONPrintingTest.cpp
@@ -17,7 +17,7 @@ TEST(JSONPrintingTest, print_truncated_message_test) {
       "{\"time_of_flight\": [\n    15579,\n    91072,\n    "
       "32972,\n    79344,\n    22827,\n    32972,\n    79344,\n    22827,\n    "
       "32972,\n    79344,\n    22827,\n    32972,\n    79344,\n    22827,\n    "
-      "37233,\n]}";
+      "37233\n]}";
   EXPECT_EQ(getTruncatedMessage(InputMessage, 4),
             "{\n    time_of_flight: [\n        15579\n        91072\n        "
             "32972\n        79344\n        22827\n        32972\n        "
@@ -28,8 +28,8 @@ TEST(JSONPrintingTest, print_truncated_message_test) {
 
 TEST(JSONPrintingTest, print_nested_maps_and_sequences_test) {
   std::string InputMessage = "[ 15579, 91072, {\n"
-                             "  32972: {\n"
-                             "    32972:\n"
+                             "  \"32972\": {\n"
+                             "    \"32972\":\n"
                              "    32972}},[15579, 91072] ]";
   EXPECT_NO_THROW(getTruncatedMessage(InputMessage, 4));
 }
