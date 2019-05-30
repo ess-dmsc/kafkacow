8d442641a3a25b43e34ebca5904861b48c71e786
mchorazak <michochozaku@gmail.com>
2019-05-30 14:54:42 +0100

    llvm

--- test/MessageTest.cpp
+++ test/MessageTest.cpp
@@ -7,12 +7,12 @@ class MessageTest : public ::testing::Test {};
 
 TEST(MessageTest, message_struct_methods_working_correctly) {
 
-  flatbuffers::FlatBufferBuilder builder;
-  builder.Clear();
+  flatbuffers::FlatBufferBuilder Builder;
+  Builder.Clear();
   std::string MessageToSerialize = "{\"SimpleJson\" : 42}";
-  auto FBOffset = CreateJsonDataDirect(builder, MessageToSerialize.c_str());
-  FinishJsonDataBuffer(builder, FBOffset);
-  auto KafkaMessage = Kafka::Message(builder.Release());
+  auto FBOffset = CreateJsonDataDirect(Builder, MessageToSerialize.c_str());
+  FinishJsonDataBuffer(Builder, FBOffset);
+  auto KafkaMessage = Kafka::Message(Builder.Release());
   std::string MessageString(KafkaMessage.data(),
                             static_cast<int>(KafkaMessage.size()));
   EXPECT_EQ(GetJsonData(MessageString.c_str())->json()->str(),
