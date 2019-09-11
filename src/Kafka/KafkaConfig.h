59d94fbb7ec5e6ad07a8bb7b1adee586ccc8b03e
Matthew D Jones <matthew.d.jones@tessella.com>
2019-09-11 16:09:10 +0100

    Update librdkafka

--- src/Kafka/KafkaConfig.h
+++ src/Kafka/KafkaConfig.h
@@ -24,11 +24,11 @@ createGlobalConfiguration(const std::string &BrokerAddr) {
 
   setConfig(*conf, "metadata.broker.list", BrokerAddr);
   setConfig(*conf, "session.timeout.ms", "10000");
-  setConfig(*conf, "message.max.bytes", "10000000");
-  setConfig(*conf, "fetch.message.max.bytes", "10000000");
+  setConfig(*conf, "message.max.bytes", "200000000");
+  setConfig(*conf, "receive.message.max.bytes", "200000512");
+  setConfig(*conf, "fetch.message.max.bytes", "200000000");
   setConfig(*conf, "enable.auto.commit", "false");
   setConfig(*conf, "enable.auto.offset.store", "false");
-  setConfig(*conf, "offset.store.method", "none");
   setConfig(*conf, "api.version.request", "true");
   setConfig(*conf, "auto.offset.reset", "largest");
   // kafkacow uses assign not subscribe, so group id is not used for consumer
