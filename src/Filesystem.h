97045df0a48b0b4ba7938ef80d6994f36359dfb3
Matthew D Jones <matthew.d.jones@tessella.com>
2020-02-22 12:27:37 +0000

    Remove unintended code from Filesystem header

--- src/Filesystem.h
+++ src/Filesystem.h
@@ -3,7 +3,6 @@
 #ifdef __cpp_lib_filesystem
 #include <filesystem>
 namespace fs = std::filesystem;
-std::cout << "Using std::filesystem\n";
 #else
 #include <experimental/filesystem>
 namespace fs = std::experimental::filesystem;
