46d8f59c301461e295c8e117355afbfa34a3a6d1
Matthew D Jones <matthew.d.jones@tessella.com>
2020-02-24 11:20:44 +0000

    Refactor GraphicalInterface

--- src/GraphicalInterface.cpp
+++ src/GraphicalInterface.cpp
@@ -1,55 +1,57 @@
 #include "GraphicalInterface.h"
 #include "Kafka/Consumer.h"
 
-#include <imgui.h>
-#include <imgui-SFML.h>
+#include <SFML/Graphics/CircleShape.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Clock.hpp>
 #include <SFML/Window/Event.hpp>
-#include <SFML/Graphics/CircleShape.hpp>
+#include <imgui-SFML.h>
+#include <imgui.h>
 #include <memory>
 
-void initGUI(std::string const &Broker)
-{
-    auto KafkaConsumer = std::make_unique<Kafka::Consumer>(Broker);
-    auto MetadataString = KafkaConsumer->showAllMetadata();
+void metadataWindow(std::unique_ptr<Kafka::Consumer> &KafkaConsumer,
+                    std::string &MetadataString) {
+  ImGui::Begin("Metadata");
 
+  auto Refresh = ImGui::Button("Refresh");
+  if (Refresh) {
+    KafkaConsumer->updateMetadata();
+    MetadataString = KafkaConsumer->showAllMetadata();
+  }
+  ImGui::Text("%s", MetadataString.c_str());
+  ImGui::End();
+}
 
+void initGUI(std::string const &Broker) {
+  auto KafkaConsumer = std::make_unique<Kafka::Consumer>(Broker);
+  auto MetadataString = KafkaConsumer->showAllMetadata();
 
-    sf::RenderWindow window(sf::VideoMode(640, 480), "kafkacow");
-    window.setFramerateLimit(60);
-    ImGui::SFML::Init(window);
+  sf::RenderWindow window(sf::VideoMode(640, 480), "kafkacow");
+  window.setFramerateLimit(60);
+  ImGui::SFML::Init(window);
 
-    sf::Clock deltaClock;
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            ImGui::SFML::ProcessEvent(event);
+  sf::Clock deltaClock;
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      ImGui::SFML::ProcessEvent(event);
 
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-        }
-
-        ImGui::SFML::Update(window, deltaClock.restart());
-
-        ImGui::Begin("Metadata");
-
-        bool ShowDemoWindow = true;
-        ImGui::ShowDemoWindow(&ShowDemoWindow);
-
-        auto Refresh = ImGui::Button("Refresh");
-        if (Refresh) {
-            KafkaConsumer->updateMetadata();
-            MetadataString = KafkaConsumer->showAllMetadata();
-        }
-        ImGui::Text("%s", MetadataString.c_str());
-        ImGui::End();
-
-        window.clear(sf::Color(0, 50, 150, 255));
-        ImGui::SFML::Render(window);
-        window.display();
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
     }
 
-    ImGui::SFML::Shutdown();
+    ImGui::SFML::Update(window, deltaClock.restart());
+
+    bool ShowDemoWindow = true;
+    ImGui::ShowDemoWindow(&ShowDemoWindow);
+
+    metadataWindow(KafkaConsumer, MetadataString);
+
+    window.clear(sf::Color(0, 50, 150, 255));
+    ImGui::SFML::Render(window);
+    window.display();
+  }
+
+  ImGui::SFML::Shutdown();
 }
