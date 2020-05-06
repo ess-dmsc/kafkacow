9b86802bb829bc074ceb47fba5ea979750aea341
Morten Jagd Christensen <morten.christensen@esss.se>
2020-05-06 10:10:32 +0200

    make compact view the default one, change indent to 2

--- src/JSONPrinting.h
+++ src/JSONPrinting.h
@@ -7,12 +7,10 @@
 std::string getEntireMessage(const std::string &JSONMessage, const int &Indent);
 
 std::string getTruncatedMessage(const std::string &JSONMessage,
-                                const int &Indent, bool CompactMessage);
+                                const int &Indent);
 
 void recursiveTruncate(nlohmann::json &JSONMessage);
 
 void recursiveTruncateJSONMap(nlohmann::json &JSONMessage);
 
 void recursiveTruncateJSONArray(nlohmann::json &JSONMessage);
-
-void recursiveCompactJSONArray(nlohmann::json &JSONMessage);
--- test/JSONPrintingTest.cpp
+++ test/JSONPrintingTest.cpp
@@ -2,9 +2,6 @@
 #include "f142_logdata_generated.h"
 #include <gtest/gtest.h>
 
-bool CompactOFF{false};
-bool CompactON{true};
-
 class JSONPrintingTest : public ::testing::Test {};
 
 TEST(JSONPrintingTest, print_entire_message_test) {
@@ -19,29 +16,23 @@ TEST(JSONPrintingTest, print_truncated_array_message_test) {
       "32972,\n    79344,\n    22827,\n    32972,\n    79344,\n    22827,\n    "
       "32972,\n    79344,\n    22827,\n    32972,\n    79344,\n    22827,\n    "
       "37233\n]}";
-  EXPECT_EQ(getTruncatedMessage(InputMessage, 4, CompactOFF),
-            "{\n    time_of_flight: [\n        15579\n        91072\n        "
-            "32972\n        79344\n        22827\n        32972\n        "
-            "79344\n        22827\n        32972\n        79344\n        "
-            "...\n        Truncated 5 elements.\n    "
-            "]\n}");
-}
-
-TEST(JSONPrintingTest, print_compacted_array_message_test) {
-  std::string InputMessage =
-      "{\"time_of_flight\": [\n    15579,\n    91072,\n    "
-      "32972,\n    79344,\n    22827,\n    32972,\n    79344,\n    22827,\n    "
-      "32972,\n    79344,\n    22827,\n    32972,\n    79344,\n    22827,\n    "
-      "37233\n]}";
-  EXPECT_EQ(getTruncatedMessage(InputMessage, 1, CompactON),
+  EXPECT_EQ(getTruncatedMessage(InputMessage, 1),
             "{\n time_of_flight: [   15579   91072   32972   "
             "... truncated 12 elements ...   37233  ]\n}");
+}
+
+TEST(JSONPrintingTest, print_truncated_short_array_message_test) {
+  std::string InputMessage =
+      "{\"time_of_flight\": [\n    15579,\n    91072,\n    "
+      "32972,\n    79344\n]}";
+  EXPECT_EQ(getTruncatedMessage(InputMessage, 1),
+            "{\n time_of_flight: [   15579   91072   32972   79344  ]\n}");
 }
 
 TEST(JSONPrintingTest, print_truncated_string_message_test) {
   std::string InputMessage =
       R"({"long_string": "This is a long string which should be truncated by kafkacow"})";
-  EXPECT_EQ(getTruncatedMessage(InputMessage, 4, CompactOFF),
+  EXPECT_EQ(getTruncatedMessage(InputMessage, 4),
             "{\n    long_string: This is a long string which should be "
             "truncated by ... Truncated 9 characters.\n}");
 }
@@ -51,7 +42,7 @@ TEST(JSONPrintingTest, print_nested_maps_and_sequences_test) {
                              "  \"32972\": {\n"
                              "    \"32972\":\n"
                              "    32972}},[15579, 91072] ]";
-  EXPECT_NO_THROW(getTruncatedMessage(InputMessage, 4, CompactOFF));
+  EXPECT_NO_THROW(getTruncatedMessage(InputMessage, 4));
 }
 
 TEST(JSONPrintingTest, print_empty_arrays_empty_maps) {
@@ -59,7 +50,7 @@ TEST(JSONPrintingTest, print_empty_arrays_empty_maps) {
                              "    \"emptyArray\":[],"
                              "    \"emptyObject\": {}"
                              "}";
-  EXPECT_NO_THROW(getTruncatedMessage(InputMessage, 4, CompactOFF));
+  EXPECT_NO_THROW(getTruncatedMessage(InputMessage, 4));
 }
 
 TEST(JSONPrintingTest, print_entire_message_empty_arrays_empty_maps) {
@@ -79,12 +70,12 @@ TEST(JSONPrintingTest, print_entire_non_json) {
 TEST(JSONPrintingTest, print_truncated_non_json) {
   std::string LongMessage = "This is a message that should be long enough to "
                             "truncate, and it is not JSON.";
-  EXPECT_TRUE(LongMessage.find(getTruncatedMessage(
-                  LongMessage, 4, CompactOFF)) != std::string::npos);
+  EXPECT_TRUE(LongMessage.find(getTruncatedMessage(LongMessage, 4)) !=
+              std::string::npos);
 }
 
 TEST(JSONPrintingTest, print_non_json_too_short_to_truncate) {
   std::string ShortMessage = "Too short";
-  EXPECT_TRUE(ShortMessage.find(getTruncatedMessage(
-                  ShortMessage, 4, CompactOFF)) != std::string::npos);
+  EXPECT_TRUE(ShortMessage.find(getTruncatedMessage(ShortMessage, 4)) !=
+              std::string::npos);
 }
